4dea53f5c4e5be86144abae601b12aaa93262f44
Krishna :) <35192026+ra5put1n@users.noreply.github.com>
2023-04-20 18:21:07 +0200

    Delete src directory

--- src/AnrModule.js
+++
@@ -1,5 +0,0 @@
-angular.module('AnrModule', ['angularResizable', 'angular-loading-bar', 'ngAnimate'])
-        .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
-        cfpLoadingBarProvider.latencyThreshold = 1000;
-        cfpLoadingBarProvider.includeSpinner = false;
-        }]);
--- src/DashNullFilter.js
+++
@@ -1,9 +0,0 @@
-angular.module('AnrModule').filter('dashnull', function () {
-    return function (input) {
-        if (input === null || input === '' || input === undefined || input < 0) {
-            return '-';
-        } else {
-            return input;
-        }
-    };
-});
